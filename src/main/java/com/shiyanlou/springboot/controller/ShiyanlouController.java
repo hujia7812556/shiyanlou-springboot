6d5815586089e2acd4f05f1429e0c782474a2086
jhu1 <jhu1@i9i8.com>
2020-06-04 01:34:23 +0800

    update

--- src/main/java/com/shiyanlou/springboot/controller/ShiyanlouController.java
+++ src/main/java/com/shiyanlou/springboot/controller/ShiyanlouController.java
@@ -1,22 +1,14 @@
 package com.shiyanlou.springboot.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
-@RestController
-// RestController 相当于同时使用 @Controller 和 @ResponseBody 注解
-// 加载 classpath 目录下的 shiyanlou.properties 文件
-@PropertySource(value = "classpath:shiyanlou.properties")
+// 注意这里已经不是 @RestController 了和上个试验不同，因为需要返回视图，所以不能使用 @ResponseBody
+@Controller
 public class ShiyanlouController{
-    // 使用 @Value 注解注入属性值
-    @Value("${shiyanlou.test}")
-    private String shiyanlou;
 
     @RequestMapping("shiyanlou")
     public String shiyanlou(){
-        return this.shiyanlou;
+        return "shiyanlou";
     }
 }
