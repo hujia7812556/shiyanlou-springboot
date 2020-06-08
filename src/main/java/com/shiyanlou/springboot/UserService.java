a1f8d23b1dac8fd41f52e349c740544fb0b98386
jhu1 <jhu1@i9i8.com>
2020-06-08 13:23:15 +0800

    update

--- src/main/java/com/shiyanlou/springboot/UserService.java
+++ src/main/java/com/shiyanlou/springboot/UserService.java
@@ -1,3 +1,4 @@
+
 package com.shiyanlou.springboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,20 +12,16 @@ public class UserService{
     @Autowired
     private UserRepository userRepository;
 
-    /**
-     * 添加事务管理
-     */
-    @Transactional(rollbackFor = Exception.class)
     public User save(User user){
         // 保存实体类
         userRepository.save(user);
-        // 人为抛出异常
-        int shiyanlou = 1 / 0;
+        // 去除异常
+//         int shiyanlou = 1 / 0;
+
         // 修改密码
         user.setPassword("123456");
         // 重新保存，更新记录
         return userRepository.save(user);
     }
-
-
 }
+
